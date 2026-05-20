31dedfdaf3ea307bea53124075c959c653efc713
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-05-21 06:41:08 +0700

    old pseudolearn finishing.11

--- resources/views/pages/ujian/index.blade.php
+++ resources/views/pages/ujian/index.blade.php
@@ -303,7 +303,7 @@
         </div>
     </div>
     @extends('pages.guide.index')
-    @extends('pages.Ujian.modal')
+    @extends('pages.ujian.modal')
     <script>
         var hostUrl = "assets/";
     </script>
