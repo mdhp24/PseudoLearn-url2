76f4101886af9494e35b46dce75e670a372279f3
Baihaqi <116696555+G4CENeiz@users.noreply.github.com>
2026-05-25 04:56:47 +0700

    chore: revert seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            UserProductionSeeder::class,
-            SoalProductionSeeder::class,
         ]);
     }
 }
